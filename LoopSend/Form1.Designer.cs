0378d19fd9454c459de907a0fa54210e21320136
EKOISMYLOVE <wiwi571246@gmail.com>
2020-08-31 22:27:06 +0800

    First commit

---
+++ LoopSend/Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿namespace LoopSend
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.bindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            this.intervalTime = new System.Windows.Forms.TextBox();
+            this.sendTime = new System.Windows.Forms.TextBox();
+            this.sendData = new System.Windows.Forms.TextBox();
+            this.selectBoard = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SendLog = new System.Windows.Forms.RichTextBox();
+            this.ReceiveLog = new System.Windows.Forms.RichTextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.disableLog = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // intervalTime
+            // 
+            this.intervalTime.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.intervalTime.Location = new System.Drawing.Point(96, 145);
+            this.intervalTime.Name = "intervalTime";
+            this.intervalTime.Size = new System.Drawing.Size(150, 29);
+            this.intervalTime.TabIndex = 0;
+            // 
+            // sendTime
+            // 
+            this.sendTime.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.sendTime.Location = new System.Drawing.Point(96, 189);
+            this.sendTime.Name = "sendTime";
+            this.sendTime.Size = new System.Drawing.Size(150, 29);
+            this.sendTime.TabIndex = 1;
+            // 
+            // sendData
+            // 
+            this.sendData.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.sendData.Location = new System.Drawing.Point(96, 240);
+            this.sendData.Name = "sendData";
+            this.sendData.Size = new System.Drawing.Size(150, 29);
+            this.sendData.TabIndex = 2;
+            // 
+            // selectBoard
+            // 
+            this.selectBoard.Font = new System.Drawing.Font("微軟正黑體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.selectBoard.FormattingEnabled = true;
+            this.selectBoard.Location = new System.Drawing.Point(96, 96);
+            this.selectBoard.Name = "selectBoard";
+            this.selectBoard.Size = new System.Drawing.Size(150, 24);
+            this.selectBoard.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(19, 100);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "開發版";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label2.Location = new System.Drawing.Point(19, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "間隔時間";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label3.Location = new System.Drawing.Point(19, 192);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "傳送時間";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.label4.Location = new System.Drawing.Point(19, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 20);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "傳送資料";
+            // 
+            // SendLog
+            // 
+            this.SendLog.Location = new System.Drawing.Point(26, 21);
+            this.SendLog.Name = "SendLog";
+            this.SendLog.Size = new System.Drawing.Size(350, 160);
+            this.SendLog.TabIndex = 8;
+            this.SendLog.Text = "";
+            // 
+            // ReceiveLog
+            // 
+            this.ReceiveLog.Location = new System.Drawing.Point(26, 24);
+            this.ReceiveLog.Name = "ReceiveLog";
+            this.ReceiveLog.Size = new System.Drawing.Size(350, 160);
+            this.ReceiveLog.TabIndex = 9;
+            this.ReceiveLog.Text = "";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.disableLog);
+            this.groupBox1.Controls.Add(this.SendButton);
+            this.groupBox1.Controls.Add(this.selectBoard);
+            this.groupBox1.Controls.Add(this.intervalTime);
+            this.groupBox1.Controls.Add(this.sendTime);
+            this.groupBox1.Controls.Add(this.sendData);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(272, 405);
+            this.groupBox1.TabIndex = 10;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "參數設定";
+            // 
+            // SendButton
+            // 
+            this.SendButton.Font = new System.Drawing.Font("微軟正黑體", 12F);
+            this.SendButton.Location = new System.Drawing.Point(149, 315);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(100, 30);
+            this.SendButton.TabIndex = 8;
+            this.SendButton.Text = "執行";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.SendLog);
+            this.groupBox2.Location = new System.Drawing.Point(290, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(400, 200);
+            this.groupBox2.TabIndex = 11;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Send";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.ReceiveLog);
+            this.groupBox3.Location = new System.Drawing.Point(290, 217);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(400, 200);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Receive";
+            // 
+            // disableLog
+            // 
+            this.disableLog.AutoSize = true;
+            this.disableLog.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.disableLog.Location = new System.Drawing.Point(23, 319);
+            this.disableLog.Name = "disableLog";
+            this.disableLog.Size = new System.Drawing.Size(120, 24);
+            this.disableLog.TabIndex = 9;
+            this.disableLog.Text = "關閉部分Log";
+            this.disableLog.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 431);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "LoopSend";
+            ((System.ComponentModel.ISupportInitialize)(this.bindingSource1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource bindingSource1;
+        private System.Windows.Forms.TextBox intervalTime;
+        private System.Windows.Forms.TextBox sendTime;
+        private System.Windows.Forms.TextBox sendData;
+        private System.Windows.Forms.ComboBox selectBoard;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox SendLog;
+        private System.Windows.Forms.RichTextBox ReceiveLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckBox disableLog;
+    }
+}
+
---
+++ LoopSend/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Management;
+using System.IO.Ports;
+using System.Threading;
+
+namespace LoopSend
+{
+    public partial class Form1 : Form
+    {
+        public string[] deviceList = { };
+
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            var searcher = new ManagementObjectSearcher("SELECT * FROM WIN32_SerialPort");
+
+            foreach (ManagementObject port in searcher.Get())
+            {
+                // ex: COM7
+                string name = port.GetPropertyValue("DeviceID").ToString();
+
+                Array.Resize(ref deviceList, deviceList.Length + 1);
+                deviceList[deviceList.Length - 1] = name;
+
+                // ex: Arduino Uno (COM7)
+                string description = port.GetPropertyValue("Caption").ToString();
+
+                //MessageBox.Show(theSerialNumberObjectQuery["SerialNumber"].ToString());
+
+                // do what ever you want...  
+                selectBoard.Items.Add(description);
+                
+            }
+            try
+            {
+                selectBoard.SelectedIndex = 0;
+            }
+            catch (Exception)
+            {
+                selectBoard.SelectedIndex = -1;
+                throw;
+            }
+            
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            SendLog.AppendText("[*] 選擇Port為 " + deviceList[selectBoard.SelectedIndex].ToString()+ " 的開發版開始傳輸" + "\r\n");
+
+            ConnectSerialDevice CSD = new ConnectSerialDevice(deviceList[selectBoard.SelectedIndex].ToString(), this);
+            CSD._receiveLog = ReceiveLog;
+
+            SendLog.AppendText("[*] 間隔時間為 : " + intervalTime.Text + "\r\n");
+            SendLog.AppendText("[*] 傳輸時間為 : " + sendTime.Text  + "\r\n");
+
+            int count = 1;
+
+            if (disableLog.Checked)
+            {
+                for (int i = 0; i < Int32.Parse(sendTime.Text); i = i + Int32.Parse(intervalTime.Text))
+                {
+                    //CSD.SendData(sendData.Text);
+                    //SendLog.AppendText("[*] 送出第" + count.ToString() + "筆資料:" + sendData.Text + "\r\n");
+                    Thread.Sleep(Int32.Parse(intervalTime.Text));
+                }
+            }
+            else
+            {
+                for (int i = 0; i < Int32.Parse(sendTime.Text); i = i + Int32.Parse(intervalTime.Text))
+                {
+                    //CSD.SendData(sendData.Text);
+                    SendLog.AppendText("[*] 送出第" + count.ToString() + "筆資料:" + sendData.Text + "\r\n");
+                    Thread.Sleep(Int32.Parse(intervalTime.Text));
+                    count++;
+                }
+            }
+
+            SendLog.AppendText("[*] 傳輸完畢。" + "\r\n");
+
+        }
+    }
+
+    public class ConnectSerialDevice
+    {
+        Form Form1;
+        private SerialPort My_SerialPort;
+        private bool Console_receiving = false;
+        private Thread t;
+
+        public RichTextBox _sendLog;
+        public RichTextBox _receiveLog;
+        delegate void Display(RichTextBox rtb, string buffer);
+
+        private string COM;
+        private Int32 baud = 115200;
+        private string buf = "";
+        public string temp = "";
+        public string address = "--------------------";//抓到要連線的address
+        public bool role = false;//master為true
+
+        public ConnectSerialDevice(string COM_Port_Number, Form myForm)
+        {
+            COM = COM_Port_Number;
+            Form1 = myForm;
+        }
+
+
+        //public void Console_Connect(string COM, Int32 baud)
+        public void Console_Connect()
+        {
+            try
+            {
+                My_SerialPort = new SerialPort();
+
+                if (My_SerialPort.IsOpen)
+                {
+                    My_SerialPort.Close();
+                }
+
+                //設定 Serial Port 參數
+                My_SerialPort.PortName = COM;
+                My_SerialPort.BaudRate = baud;
+                My_SerialPort.DataBits = 8;
+                My_SerialPort.StopBits = StopBits.One;
+
+                if (!My_SerialPort.IsOpen)
+                {
+                    //開啟 Serial Port
+                    My_SerialPort.Open();
+
+                    Console_receiving = true;
+
+                    //開啟執行續做接收動作
+                    t = new Thread(DoReceive);
+                    t.IsBackground = true;
+                    t.Start();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void DoReceive()
+        {
+            Byte[] buffer = new Byte[1024];
+
+            try
+            {
+                while (Console_receiving)
+                {
+                    if (My_SerialPort.BytesToRead > 0)
+                    {
+                        Int32 length = My_SerialPort.Read(buffer, 0, buffer.Length);
+
+                        //string buf = Encoding.ASCII.GetString(buffer);
+                        this.buf = Encoding.ASCII.GetString(buffer);
+
+
+                        Array.Resize(ref buffer, length);
+                        Display d = new Display(ConsoleShow);
+                        Form1.Invoke(d, new Object[] { _receiveLog, buf });
+                        Array.Resize(ref buffer, 1024);
+
+                    }
+                    //Thread.Sleep(20);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public void ConsoleShow(RichTextBox rtb, string buffer)
+        {
+
+            string target = "slave1";//要判斷的名稱
+
+            if (role)
+            {
+                address = MasterGetMACAddress(target, buffer);
+                if (address.Length == 26)
+                {
+                    rtb.AppendText("\r\n[*]取得 " + target + "的 MAC address : " + address + "\r\n");
+
+                    SendData(Encoding.ASCII.GetBytes("bt connect " + address + "\n"));
+                    address = "";
+                }
+            }
+
+            rtb.AppendText(buffer);
+            /*
+            bool b = buffer.Contains(target);//判斷是否有找到目標字串
+            if (b)
+            {
+                int index = buffer.IndexOf(target);//index為找到字串的起始位置
+                if (index > 52)//避免抓到bt name xxxxx的case
+                {
+                    address = buffer.Substring(index - 52, 26);//我人工判讀要抓的字串位置以及長度
+                    if(role)//如果為master要執行
+                        SendData(Encoding.ASCII.GetBytes("bt connect " + address + "\r"));
+                }
+            }
+            */
+
+            //rtb.AppendText(buffer);
+            //rtb.ScrollToCaret();
+            //rtb.AppendText("[*]取得 " + target + " MAC address : " + address + "\r\n");
+        }
+
+        public string MasterGetMACAddress(string slaveName, string buffer)
+        {
+            string target = slaveName;//要判斷的名稱
+            string scanResult = buffer;
+            string slaveAddress = "";
+
+            bool b = scanResult.Contains(target);//判斷是否有找到目標字串
+            if (b)
+            {
+                int index = scanResult.IndexOf(target);//index為找到字串的起始位置
+                if (index > 52)//避免抓到bt name xxxxx的case
+                {
+                    slaveAddress = scanResult.Substring(index - 52, 26);//我人工判讀要抓的字串位置以及長度                        
+                }
+            }
+            return slaveAddress;
+        }
+
+        public void SendData(Object sendBuffer)
+        {
+
+            if (sendBuffer != null)
+            {
+                Byte[] buffer = sendBuffer as Byte[];
+
+                try
+                {
+                    My_SerialPort.Write(buffer, 0, buffer.Length);
+                }
+                catch (Exception ex)
+                {
+                    CloseComport();
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        public void CloseComport()
+        {
+            try
+            {
+                My_SerialPort.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        public string getBuffer()
+        {
+
+            return this.temp;
+        }
+
+
+    }
+}
